df2d6422d9fe54449f17d2717599ca60687de42c
viveksoni100 <viveksoni100@gmail.com>
2023-03-16 01:31:22 +0530

    quiz 4 added

--- js/quiz.js
+++ js/quiz.js
@@ -1,117 +1,147 @@
-//Quiz#2
 $('#quiz').quiz({
     counterFormat: 'Question %current of %total',
     questions: [
         {
-            'q': 'What is the output of the following?\n\n'+
-                '1: public class Legos {\n' +
-                '2: ' +
-                'public static void main(String[] args) {\n' +
-                '3: ' +
-                'var sb = new StringBuilder();\n' +
-                '4: ' +
-                'sb.append("red");\n' +
-                '5: ' +
-                'sb.deleteCharAt(0);\n' +
-                '6: ' +
-                'sb.delete(1, 2);\n' +
-                '7: ' +
-                'System.out.println(sb);\n' +
-                '8:' +
+            'q': 'What does the following code snippet output?\n\n'+
+                'var bottles = List.of("glass", "plastic", "can");\n' +
+                'for (int type = 1; type < bottles.size();) {\n' +
+                'System.out.print(bottles.get(type) + "-");\n' +
+                'if(type < bottles.size()) break;\n' +
                 '}\n' +
-                '9: }',
+                'System.out.print("end");',
             'options': [
-                'e',
-                'd',
-                'ed',
-                'None of the above'
-            ],
-            'correctIndex': 0,
-            'correctResponse': 'Correct ✅',
-            'incorrectResponse': '❌ A. In the first expression, height > 1 is true. Since it uses the logical operator (|), which\n' +
-                'evaluates both sides, the right side is still executed, resulting in length being assigned a\n' +
-                'value of 2 and w assigned a value of true. In the second expression, only the right-hand side\n' +
-                'of the expression is evaluated, so x is assigned a value of 2, and length is unchanged. The\n' +
-                'last expression evaluates to 2 % 2, which is 0, so z is assigned a value of 0.'
-        },
-        {
-            'q': 'Which is a true statement?\n\n' +
-                'A.If s.contains("abc") is true, then s.equals("abc") is also true.\n' +
-                'B.If s.contains("abc") is true, then s.startsWith("abc") is also true.\n' +
-                'C.If s.startsWith("abc") is true, then s.equals("abc") is also true.\n' +
-                'D.If s.startsWith("abc") is true, then s.contains("abc") is also true.',
-            'options': [
-                'A',
-                'B',
-                'C',
-                'D',
-                'None of the above'
-            ],
-            'correctIndex': 3,
-            'correctResponse': 'Good ✅',
-            'incorrectResponse': '❌ D. Options A and B are not true if the String is "deabc". Option C is not true if the\n' +
-                'String is "abcde". Option D is true in all cases.'
-        },
-        {
-            'q': 'What is the output of the following class?\n\n' +
-                '1: package rocket;\n' +
-                '2: public class Countdown {\n' +
-                '3: ' +
-                'public static void main(String[] args) {\n' +
-                '4: ' +
-                'var builder = new StringBuilder("54321");\n' +
-                '5: ' +
-                'builder.substring(2);\n' +
-                '6: ' +
-                'System.out.println(builder.charAt(1));\n' +
-                '7: ' +
-                '}\n' +
-                '8: }'
-            ,
-            'options': [
-                '1',
-                '2',
-                '3',
-                '4',
-                'Does not compile'
-            ],
-            'correctIndex': 3,
-            'correctResponse': 'Excellent ✅',
-            'incorrectResponse': '❌ D. Line 4 creates a StringBuilder of length 5. Pay attention to the substring()\n' +
-                'method in StringBuilder. It returns a String with the value 321. It does not change\n' +
-                'the StringBuilder itself. Then line 6 retrieves the second indexed element from that\n' +
-                'unchanged value, which is 4. Therefore, option D is correct.'
-        },
-        {
-            'q': 'Variables declared as which of the following are never permitted in\na switch statement?\n\n',
-            'options': [
-                'A.var',
-                'B.double',
-                'C.int',
-                'D.String',
-                'E.char'
-            ],
-            'correctIndex': 1,
-            'correctResponse': 'Bravo ✅',
-            'incorrectResponse': '❌ B, a switch statement supports the primitive types byte, short, char, and int and\n' +
-                'the wrapper classes Character, Byte, Short, and Integer. It also supports String\n' +
-                'and enumerated types. Finally, it permits var if it can be resolved to one of the previous\n' +
-                'types. Floating-point types like float and double are not supported; therefore, option B\n' +
-                'is correct.'
-        },
-        {
-            'q': 'Which of the following rules about a default branch in a switch statement are correct?\n\n',
-            'options': [
-                'A.A switch statement is required to declare a default statement.',
-                'B.A default statement must be placed after all case statements.',
-                'C.A default statement can be placed between any case statements.',
-                'D.A switch statement can contain more than one default statement.'
+                'A.glass-end',
+                'B.glass-plastic-can-end',
+                'C.plastic-end',
+                'D.plastic-can-end',
+                'E.The code does not compile.'
             ],
             'correctIndex': 2,
+            'correctResponse': 'Correct ✅',
+            'incorrectResponse': '❌ C. The first time through the loop, type is 1, and plastic- is output. The break\n' +
+                'statement then terminates the loop, with end bring printed. If the break keyword was\n' +
+                'removed, then this would be an infinite loop because type is not incremented between loop\n' +
+                'executions.'
+        },
+        {
+            'q': 'What is the result of executing the following code snippet?\n\n' +
+                'final var GOOD = 100;\n' +
+                'var score = 10;\n' +
+                'switch (score) {\n' +
+                'default:\n' +
+                '1 : System.out.print("1-");\n' +
+                '-1 : System.out.print("2-"); break;\n' +
+                '4,5 : System.out.print("3-");\n' +
+                '6 : System.out.print("4-");\n' +
+                '9 : System.out.print("5-");\n' +
+                '}',
+            'options': [
+                'A.1-',
+                'B.1-2-',
+                'C.2-',
+                'D.3-',
+                'E.4-',
+                'F.None of the above'
+            ],
+            'correctIndex': 5,
+            'correctResponse': 'Good ✅',
+            'incorrectResponse': '❌ F. The code does not compile because the switch statement is missing the case keyword\n' +
+                'for each value. Also, two case values cannot be combined as in 4,5.'
+        },
+        {
+            'q': 'What is the output of the following application?\n\n' +
+                'package dinosaur;\n' +
+                'public class Park {\n' +
+                'public final static void main(String... arguments) {\n' +
+                'int pterodactyl = 8;\n' +
+                'long triceratops = 3;\n' +
+                'if(pterodactyl % 3 > 1 + 1)\n' +
+                'triceratops++;\n' +
+                'triceratops--;\n' +
+                'System.out.print(triceratops);\n' +
+                '}\n' +
+                '}'
+            ,
+            'options': [
+                'A.2',
+                'B.3',
+                'C.4',
+                'D.The code does not compile.',
+                'E.The code compiles but throws an exception at runtime.'
+            ],
+            'correctIndex': 0,
+            'correctResponse': 'Excellent ✅',
+            'incorrectResponse': '❌ A. First, determine whether the if statement’s expression is executed. The expression\n' +
+                '8 % 3 evaluates to 2. The right-hand side of the expression is evaluated next since + has a\n' +
+                'higher precedence than >. Since 2 > 2 is false, the expression triceratops++ is not\n' +
+                'called. Notice there are no braces, {}, in the if statement. Despite the triceratops–– line\n' +
+                'being indented, it is not part of the if statement. Therefore, triceratops–– is always exe-\n' +
+                'cuted, resulting in a value of 2 for triceratops, and making option A the correct answer.'
+        },
+        {
+            'q': 'What variable type of red allows the following application to compile?\n\n' +
+                'package tornado;\n' +
+                'public class Kansas {\n' +
+                'public static void main(String[] args) {\n' +
+                'int colorOfRainbow = 10;\n' +
+                '____________ red = 5;\n' +
+                'switch(colorOfRainbow) {\n' +
+                'default:\n' +
+                'System.out.print("Home");\n' +
+                'break;\n' +
+                'case red:\n' +
+                'System.out.print("Away");\n' +
+                '}\n' +
+                '}\n' +
+                '}',
+            'options': [
+                'A.long',
+                'B.double',
+                'C.int',
+                'D.var',
+                'E.String',
+                'F.None of the above'
+            ],
+            'correctIndex': 5,
+            'correctResponse': 'Bravo ✅',
+            'incorrectResponse': '❌ F. The value of a case statement must be a constant, a literal value, or a final variable.\n' +
+                'Since red is missing the final attribute, no variable type allows the code to compile, mak-\n' +
+                'ing option F the correct answer. If the final modifier was added to the declaration of red,\n' +
+                'then int or var would be correct. The other options use types that are incompatible with\n' +
+                'switch statements or with colorOfRainbow.'
+        },
+        {
+            'q': 'How many lines of the magic() method contain compilation errors?\n\n' +
+                '10: public void magic() {\n' +
+                '11: ' +
+                'do {\n' +
+                '12: ' +
+                'int trick = 0;\n' +
+                '13: ' +
+                'LOOP: do {\n' +
+                '14: ' +
+                'trick++;\n' +
+                '15: ' +
+                '} while (trick < 2--);\n' +
+                '16: ' +
+                'continue LOOP;\n' +
+                '17: ' +
+                '} while (1 > 2);\n' +
+                '18: ' +
+                'System.out.println(trick);\n' +
+                '19: }',
+            'options': [
+                'A.Zero',
+                'B.One',
+                'C.Two',
+                'D.Three'
+            ],
+            'correctIndex': 3,
             'correctResponse': 'Perfect ✅',
-            'incorrectResponse': '❌ C, a default statement inside a switch statement is optional and can be placed in any\n' +
-                'order within the switch’s case statements, making options A,B and D incorrect and option\n' +
-                'C correct.'
+            'incorrectResponse': '❌ D. Line 15 does not compile because the post-decrement operator can be applied only to var-\n' +
+                'iables, not values. Line 16 also does not compile because the label LOOP is out of scope after\n' +
+                'line 15. Finally, line 18 does not compile because trick is declared within the do/while\n' +
+                'loop and out of scope after line 17. For these reasons, option D is the correct answer.'
         }
     ]
 });
