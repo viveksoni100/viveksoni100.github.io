44540541cd59e8664859214113737ce57b3373d2
viveksoni100 <viveksoni100@gmail.com>
2023-03-20 23:47:39 +0530

    quiz 5 added

--- js/quiz.js
+++ js/quiz.js
@@ -2,146 +2,124 @@ $('#quiz').quiz({
     counterFormat: 'Question %current of %total',
     questions: [
         {
-            'q': 'What does the following code snippet output?\n\n'+
-                'var bottles = List.of("glass", "plastic", "can");\n' +
-                'for (int type = 1; type < bottles.size();) {\n' +
-                'System.out.print(bottles.get(type) + "-");\n' +
-                'if(type < bottles.size()) break;\n' +
+            'q': 'How many of these statements can be inserted\n' +
+                'after the println to have the code flow from line no. 17 to 15\n' +
+                'in this diagram?\n\n'+
+                'break;\n' +
+                'break letters;\n' +
+                'break numbers;\n' +
+                'continue;\n' +
+                'continue letters;\n' +
+                'continue numbers;\n' +
+                '15: letters: for (char ch = \'a\'; ch <= \'z\'; ch++) {\n' +
+                '16: numbers: for (int n = 0; n<=10; n++) {\n' +
+                '17: System.out.print(ch);\n' +
                 '}\n' +
-                'System.out.print("end");',
+                '}',
             'options': [
-                'A.glass-end',
-                'B.glass-plastic-can-end',
-                'C.plastic-end',
-                'D.plastic-can-end',
-                'E.The code does not compile.'
+                'A.One',
+                'B.Two',
+                'C.Three',
+                'D.Four',
+                'E.Five'
             ],
             'correctIndex': 2,
             'correctResponse': 'Correct ✅',
-            'incorrectResponse': '❌ C. The first time through the loop, type is 1, and plastic- is output. The break\n' +
-                'statement then terminates the loop, with end bring printed. If the break keyword was\n' +
-                'removed, then this would be an infinite loop because type is not incremented between loop\n' +
-                'executions.'
-        },
-        {
-            'q': 'What is the result of executing the following code snippet?\n\n' +
-                'final var GOOD = 100;\n' +
-                'var score = 10;\n' +
-                'switch (score) {\n' +
-                'default:\n' +
-                '1 : System.out.print("1-");\n' +
-                '-1 : System.out.print("2-"); break;\n' +
-                '4,5 : System.out.print("3-");\n' +
-                '6 : System.out.print("4-");\n' +
-                '9 : System.out.print("5-");\n' +
-                '}',
-            'options': [
-                'A.1-',
-                'B.1-2-',
-                'C.2-',
-                'D.3-',
-                'E.4-',
-                'F.None of the above'
-            ],
-            'correctIndex': 5,
-            'correctResponse': 'Good ✅',
-            'incorrectResponse': '❌ F. The code does not compile because the switch statement is missing the case keyword\n' +
-                'for each value. Also, two case values cannot be combined as in 4,5.'
+            'incorrectResponse': '❌ C. If the code follows the arrow, then it prints each letter once, breaking out of the inner\n' +
+                'loop on every iteration. Since a break without a label applies to the innermost structure,\n' +
+                'break and break numbers are equivalent, and both of these are correct answers.\n' +
+                'Likewise, continue and continue numbers are both equivalent although both wrong\n' +
+                'in this case since they resume operation of the inner loop. That leaves break letters and\n' +
+                'continue letters. In this case, break letters stops the outer loop after printing just\n' +
+                'one letter, so it is incorrect. On the other hand, continue letters exits the inner loop\n' +
+                'and returns control to the outer loop, which is the desired behavior. Since three statements\n' +
+                'are correct, option C is correct.'
         },
         {
             'q': 'What is the output of the following application?\n\n' +
-                'package dinosaur;\n' +
-                'public class Park {\n' +
-                'public final static void main(String... arguments) {\n' +
-                'int pterodactyl = 8;\n' +
-                'long triceratops = 3;\n' +
-                'if(pterodactyl % 3 > 1 + 1)\n' +
-                'triceratops++;\n' +
-                'triceratops--;\n' +
-                'System.out.print(triceratops);\n' +
+                'package dessert;\n' +
+                'public class IceCream {\n' +
+                'public final static void main(String... args) {\n' +
+                'var flavors = 30;\n' +
+                'int eaten = 0;\n' +
+                'switch(flavors) {\n' +
+                'case 30: eaten++;\n' +
+                'case 40: eaten+=2;\n' +
+                'default: eaten--;\n' +
                 '}\n' +
-                '}'
-            ,
-            'options': [
-                'A.2',
-                'B.3',
-                'C.4',
-                'D.The code does not compile.',
-                'E.The code compiles but throws an exception at runtime.'
-            ],
-            'correctIndex': 0,
-            'correctResponse': 'Excellent ✅',
-            'incorrectResponse': '❌ A. First, determine whether the if statement’s expression is executed. The expression\n' +
-                '8 % 3 evaluates to 2. The right-hand side of the expression is evaluated next since + has a\n' +
-                'higher precedence than >. Since 2 > 2 is false, the expression triceratops++ is not\n' +
-                'called. Notice there are no braces, {}, in the if statement. Despite the triceratops–– line\n' +
-                'being indented, it is not part of the if statement. Therefore, triceratops–– is always exe-\n' +
-                'cuted, resulting in a value of 2 for triceratops, and making option A the correct answer.'
-        },
-        {
-            'q': 'What variable type of red allows the following application to compile?\n\n' +
-                'package tornado;\n' +
-                'public class Kansas {\n' +
-                'public static void main(String[] args) {\n' +
-                'int colorOfRainbow = 10;\n' +
-                '____________ red = 5;\n' +
-                'switch(colorOfRainbow) {\n' +
-                'default:\n' +
-                'System.out.print("Home");\n' +
-                'break;\n' +
-                'case red:\n' +
-                'System.out.print("Away");\n' +
-                '}\n' +
+                'System.out.print(eaten);\n' +
                 '}\n' +
                 '}',
             'options': [
-                'A.long',
-                'B.double',
-                'C.int',
-                'D.var',
-                'E.String',
-                'F.None of the above'
+                'A.1',
+                'B.2',
+                'C.3',
+                'D.The code does not compile because var cannot be used in a switch statement.',
+                'E.The code does not compile for another reason.',
+                'F.None of the above.'
             ],
-            'correctIndex': 5,
-            'correctResponse': 'Bravo ✅',
-            'incorrectResponse': '❌ F. The value of a case statement must be a constant, a literal value, or a final variable.\n' +
-                'Since red is missing the final attribute, no variable type allows the code to compile, mak-\n' +
-                'ing option F the correct answer. If the final modifier was added to the declaration of red,\n' +
-                'then int or var would be correct. The other options use types that are incompatible with\n' +
-                'switch statements or with colorOfRainbow.'
+            'correctIndex': 1,
+            'correctResponse': 'Good ✅',
+            'incorrectResponse': '❌ B. The code compiles without issue, so options D and E are incorrect. A var can be used in a\n' +
+                'switch statement, provided the underlying type resolves to a supported switch type. Next,\n' +
+                'notice there are no break statements. Once the matching case statement, 30, is reached, all\n' +
+                'remaining case statements will be executed. The variable eaten is increased by 1, then 2,\n' +
+                'then reduced by 1, resulting in a final value of 2, making option B the correct answer.'
         },
         {
-            'q': 'How many lines of the magic() method contain compilation errors?\n\n' +
-                '10: public void magic() {\n' +
-                '11: ' +
-                'do {\n' +
-                '12: ' +
-                'int trick = 0;\n' +
-                '13: ' +
-                'LOOP: do {\n' +
-                '14: ' +
-                'trick++;\n' +
-                '15: ' +
-                '} while (trick < 2--);\n' +
-                '16: ' +
-                'continue LOOP;\n' +
-                '17: ' +
-                '} while (1 > 2);\n' +
-                '18: ' +
-                'System.out.println(trick);\n' +
-                '19: }',
+            'q': 'Which of the following statements compile and create infinite loops at runtime?\n',
             'options': [
-                'A.Zero',
-                'B.One',
-                'C.Two',
-                'D.Three'
+                'A.do {}',
+                'B.for( : ) {}',
+                'C.while {}',
+                'D.for( ; ; ) {}'
             ],
             'correctIndex': 3,
+            'correctResponse': 'Excellent ✅',
+            'incorrectResponse': '❌ D. A while loop and do/while loop both require a boolean expression, making\n' +
+                'options A and C incorrect. Option C is incorrect because a for-\n' +
+                'each statement requires an assignment type and an object to iterate on. Option D is correct\n' +
+                'and shows a traditional for loop with no arguments.'
+        },
+        {
+            'q': 'Which of the following iterates a different number of times than the others?\n',
+            'options': [
+                'A.for (int k=0; k < 5; k++) {}',
+                'B.for (int k=1; k <= 5; k++) {}',
+                'C.int k=0; do { } while(k++ < 5);',
+                'D.int k=0; while (k++ < 5) {}',
+                'E.All of these iterate the same number of times.'
+            ],
+            'correctIndex': 2,
+            'correctResponse': 'Bravo ✅',
+            'incorrectResponse': '❌ C. Option A goes through five indexes on the iterations: 0, 1, 2, 3, and 4. Option B also goes\n' +
+                'through five indexes: 1, 2, 3, 4, and 5. Option D goes through five iterations as well, from 0\n' +
+                'to 4. However, option C goes through six iterations since the loop condition is at the end of\n' +
+                'the loop. Therefore, it is not like the others, and option C is the correct answer.'
+        },
+        {
+            'q': 'What is the output of the following code snippet?\n\n' +
+                'int count = 0;\n' +
+                'var stops = new String[] { "Washington", "Monroe",\n' +
+                '"Jackson", "LaSalle" };\n' +
+                'while (count < stops.length)\n' +
+                'if (stops[++count].length() < 8)\n' +
+                'break;\n' +
+                'else continue;\n' +
+                'System.out.println(count);',
+            'options': [
+                'A.0',
+                'B.1',
+                'C.2',
+                'D.3',
+                'E.The code does not compile.',
+                'F.None of the above.'
+            ],
+            'correctIndex': 1,
             'correctResponse': 'Perfect ✅',
-            'incorrectResponse': '❌ D. Line 15 does not compile because the post-decrement operator can be applied only to var-\n' +
-                'iables, not values. Line 16 also does not compile because the label LOOP is out of scope after\n' +
-                'line 15. Finally, line 18 does not compile because trick is declared within the do/while\n' +
-                'loop and out of scope after line 17. For these reasons, option D is the correct answer.'
+            'incorrectResponse': '❌ B. On the first iteration of the loop, stops[++count] evaluates to stops[1], while also\n' +
+                'setting count to 1. Since stops[1] is Monroe and it has six characters, the break is\n' +
+                'reached, and the loop stops. For this reason, 1 is printed, making option B correct.'
         }
     ]
 });
