9bea346b5cb940b06f7432ba09b26241b1100308
viveksoni100 <viveksoni100@gmail.com>
2023-03-29 21:33:00 +0530

    Q&A section added

--- js/qna.js
+++ js/qna.js
@@ -2,130 +2,93 @@ $('#quiz').quiz({
     counterFormat: 'Question %current of %total',
     questions: [
         {
-            'q': 'What is the output of the following code snippet?\n\n' +
-                'int hops = 0;\n' +
-                'int jumps = 0;\n' +
-                'jumps = hops++;\n' +
-                'if(jumps)\n' +
-                'System.out.print("Jump!");\n' +
-                'else\n' +
-                'System.out.print("Hop!");',
+            'q': 'What is the difference between\n' +
+                'JDK and JRE?',
             'options': [
-                'A.Jump!',
-                'B.Hop!',
-                'C.The code does not compile.',
-                'D.The code compiles but throws an exception at runtime.',
-                'E.None of the above.'
+                'Show answer'
             ],
-            'correctIndex': 2,
-            'correctResponse': 'Correct ✅',
-            'incorrectResponse': '❌ C. The statement if(jumps) evaluates to if(0), and since 0 is not a boolean value, the\n' +
-                'code does not compile. Java requires boolean expressions in if statements.'
+            'correctIndex': 0,
+            'correctResponse': 'JDK stands for Java Development Kit. It contains the tools and\n' +
+                'libraries for development of Java programs. It also contains\n' +
+                'compilers and debuggers needed to compile Java program,\n' +
+                'JRE stands for Java Runtime Environment. This is included in JDK.\n' +
+                'JRE provides libraries and JVM that is required to run a Java\n' +
+                'program.',
+            'incorrectResponse': ''
         },
         {
-            'q': 'Which of the following best describes the flow of\nexecution in this for loop if beta always\n' +
-                'returns false?\n\n' +
-                'for (alpha; beta; gamma) {\n' +
-                'delta;\n' +
-                '}',
+            'q': 'What is Java Virtual Machine (JVM)?',
             'options': [
-                'A.alpha',
-                'B.alpha, beta',
-                'C.alpha, beta, gamma',
-                'D.alpha, gamma',
-                'E.alpha, gamma, beta',
-                'F.None of the above'
+                'Show answer'
             ],
-            'correctIndex': 1,
-            'correctResponse': 'Good ✅',
-            'incorrectResponse': '❌ B. The initializer, which is alpha, runs first. Then Java checks the condition, which is beta,\n' +
-                'to see whether loop execution should start. Since beta returns false, the loop is never\n' +
-                'entered, and option B is correct.'
+            'correctIndex': 0,
+            'correctResponse': 'Java Virtual Machine (JVM) is an abstract machine that executes\n' +
+                'Java Bytecode. There are different JVM for different hardware and\n' +
+                'software platforms. So JVM is platform dependent. JVM is\n' +
+                'responsible for loading, verifying and executing the Bytecode on a\n' +
+                'platform.',
+            'incorrectResponse': ''
         },
         {
-            'q': 'Which of the following best describes the flow of\nexecution in this for loop if beta always\n' +
-                'returns false?\n' +
-                'boolean balloonInflated = false;\n' +
-                'do {\n' +
-                'if (!balloonInflated) {\n' +
-                'balloonInflated = true;\n' +
-                'System.out.print("inflate-");\n' +
-                '}\n' +
-                '} while (! balloonInflated);\n' +
-                'System.out.println("done");',
+            'q': 'What are the different types of\n' +
+                'memory areas allocated by JVM?',
             'options': [
-                'A.done',
-                'B.inflate-done',
-                'C.The code does not compile.',
-                'D.This is an infinite loop.',
-                'E.None of the above.'
+                'Show answer'
             ],
-            'correctIndex': 1,
-            'correctResponse': 'Excellent ✅',
-            'incorrectResponse': '❌ B. On the first iteration of the loop, the if statement executes and prints inflate-.\n' +
-                'Then the loop condition is checked. The variable balloonInflated is true, so the loop\n' +
-                'condition is false, and the loop completes and done is printed.'
+            'correctIndex': 0,
+            'correctResponse': 'In java, JVM allocates memory to different processes, methods and\n' +
+                'objects. Some of the memory areas allocated by JVM are:\n' +
+                '1. ClassLoader: It is a component of JVM used to load class\n' +
+                'files.\n' +
+                '2. Class (Method) Area: It stores per-class structures such as\n' +
+                'the runtime constant pool, field and method data, and the\n' +
+                'code for methods.\n' +
+                '3. Heap: Heap is created a runtime and it contains the runtime\n' +
+                'data area in which objects are allocated.\n' +
+                '4. Stack: Stack stores local variables and partial results at\n' +
+                'runtime. It also helps in method invocation and return\n' +
+                'value. Each thread creates a private JVM stack at the time\n' +
+                'of thread creation.\n' +
+                '5. Program Counter Register: This memory area contains the\n' +
+                'address of the Java virtual machine instruction that is\n' +
+                'currently being executed.\n' +
+                '6. Native Method Stack: This area is reserved for all the\n' +
+                'native methods used in the application.',
+            'incorrectResponse': ''
         },
         {
-            'q': 'Which of these code snippets behaves differently from the others?\n',
+            'q': 'What is JIT compiler?',
             'options': [
-                'A.\n' +
-                'if (numChipmunks == 1)\n' +
-                'System.out.println("One chipmunk");\n' +
-                'if (numChipmunks == 2)\n' +
-                'System.out.println("Two chipmunks");\n' +
-                'if (numChipmunks == 3)\n' +
-                'System.out.println("Three chipmunks");',
-                'B.\n' +
-                'switch (numChipmunks) {\n' +
-                'case 1: System.out.println("One chipmunk");\n' +
-                'case 2: System.out.println("Two chipmunks");\n' +
-                'case 3: System.out.println("Three chipmunks");\n' +
-                '}',
-                'C.\n' +
-                'if (numChipmunks == 1)\n' +
-                'System.out.println("One chipmunk");\n' +
-                'else if (numChipmunks == 2)\n' +
-                'System.out.println("Two chipmunks");\n' +
-                'else if (numChipmunks == 3)\n' +
-                'System.out.println("Three chipmunks");',
-                'D.\n' +
-                'All three code snippets do the same thing.'
+                'Show answer'
             ],
-            'correctIndex': 1,
-            'correctResponse': 'Bravo ✅',
-            'incorrectResponse': '❌ B. Options A and C print one line if numChipmunks is 1, 2, or 3. Option B does not behave\n' +
-                'the same way if numChipmunks is 1 or 2. There is no break statement, so the case state-\n' +
-                'ments fall through, and more than one statement will be printed.'
+            'correctIndex': 0,
+            'correctResponse': 'Just In Time compiler also known as JIT compiler is used for\n' +
+                'performance improvement in Java. It is enabled by default. It is\n' +
+                'compilation done at execution time rather earlier.\n' +
+                'Java has popularized the use of JIT compiler by including it in\n' +
+                'JVM.',
+            'incorrectResponse': ''
         },
         {
-            'q': 'Given the following enum and class, which option\nfills in the blank and allows the code\n' +
-                'to compile?\n\n' +
-                'enum Season { SPRING, SUMMER, WINTER }\n' +
-                'public class Weather {\n' +
-                'public int getAverageTemperate(Season s) {\n' +
-                'switch (s) {\n' +
-                'default:\n' +
-                '____________ return 30;\n' +
-                '}\n' +
-                '}\n' +
-                '}',
+            'q': 'How does ClassLoader work in\n' +
+                'Java?',
             'options': [
-                'A.case Season.WINTER:',
-                'B.case WINTER, SPRING:',
-                'C.case SUMMER | WINTER:',
-                'D.case SUMMER ->',
-                'E.case FALL:',
-                'F.None of the above'
+                'Show answer'
             ],
-            'correctIndex': 5,
-            'correctResponse': 'Perfect ✅',
-            'incorrectResponse': '❌ F. Option A is incorrect because the enum type Season is not used within a case statement.\n' +
-                'If it were just case WINTER:, then it would compile. Options B and C do not compile\n' +
-                'because switch statements do not support multiple values within a case statement. Option\n' +
-                'D is incorrect because -> is used instead of :. Option E is incorrect because FALL is not\n' +
-                'defined in the list of values for the enum Season. Since none of the lines of code is correct,\n' +
-                'option F is correct.'
+            'correctIndex': 0,
+            'correctResponse': 'In Java, ClassLoader is a class that is used to load files in JVM.\n' +
+                'ClassLoader loads files from their physical file locations e.g.\n' +
+                'Filesystem, Network location etc.\n' +
+                'There are three main types of ClassLoaders in Java.\n' +
+                '1. Bootstrap ClassLoader: This is the first ClassLoader. It\n' +
+                'loads classes from rt.jar file.\n' +
+                '2. Extension ClassLoader: It loads class files from jre/lib/ext\n' +
+                'location.\n' +
+                '3. Application ClassLoader: This ClassLoader depends on\n' +
+                'CLASSPATH to find the location of class files. If you\n' +
+                'specify your jars in CLASSPATH, then this ClassLoader\n' +
+                'will load them.',
+            'incorrectResponse': ''
         }
     ]
 });
