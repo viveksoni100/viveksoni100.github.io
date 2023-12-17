b59d414e720204e6317336c6e4d424bba41398b1
viveksoni100 <viveksoni100@gmail.com>
2023-12-17 13:59:08 +0530

    qna#5 added

--- js/qna.js
+++ js/qna.js
@@ -2,96 +2,78 @@ $('#quiz').quiz({
     counterFormat: 'Question %current of %total',
     questions: [
         {
-            'q': 'In OOPS, what is meant by composition?',
+            'q': 'Why it is not possible to do method overloading by changing\n' +
+                'return type of method in java?\n',
             'options': [
                 'Show answer'
             ],
             'correctIndex': 0,
-            'correctResponse': 'In object-oriented programming (OOP), composition is a design pattern\n' +
-             'where a class is made up of one or more instances of other classes.\n'+
-             'It is a way to create complex objects by combining simpler objects,\n'+
-             'rather than inheriting all of their properties and methods from\n'+
-             'a single parent class.\n\n'+
-
-             'For example, consider a car object that is composed of an engine object,\n'+
-             'a wheel object, and a chassis object. Each of these components can be\n'+
-             'implemented as a separate class. The car class would have properties\n'+
-             'and methods that interact with these component objects to provide functionality\n'+
-             'such as accelerating, braking, and steering.\n\n'+
-
-             'Another example could be a computer system that is composed of various\n'+
-             'components such as a CPU, RAM, hard drive, motherboard, etc. Each of these\n'+
-             'components can be implemented as a separate class, and the computer class\n'+
-             'would be composed of these component objects.\n\n'+
-
-             'Composition is often used in OOP to create modular and reusable code,\n'+
-             'as it allows for a more flexible and dynamic approach to designing objects.\n'+
-             'By separating functionality into smaller, more specialized classes,\n'+
-             'it becomes easier to modify or extend the behavior of the larger composite\n'+
-             'class without affecting the other components.',
+            'correctResponse': 'If we change the return type of overloaded methods then it will lead\n' +
+                'to ambiguous behavior. How will clients know which method will\n' +
+                'return what type. Due to this different return type are not allowed in\n' +
+                'overloaded methods.',
             'incorrectResponse': ''
         },
         {
-            'q': 'How aggregation and composition are different concepts?',
+            'q': 'Are we allowed to override a static method in Java?\n',
             'options': [
                 'Show answer'
             ],
             'correctIndex': 0,
-            'correctResponse': 'In OOPS, Aggregation and Composition are the types of association\n' +
-                'relations. A composition is a strong relationship. If the composite\n' +
-                'object is destroyed, then all its parts are destroyed. E.g. A Car has a\n' +
-                'Steering Wheel. If Car object is destroyed, then there is no meaning\n' +
-                'of Steering Wheel.\n\n' +
-                'In Aggregation, the relationship is weaker than Composition.\n\n' +
-                'E.g. A Library has students. If a Library is destroyed, Students still\n' +
-                'exist. So Library and Student are related by Aggregation. A Library\n' +
-                'has Books. If Library is destroyed, the Books are also destroyed.\n' +
-                'Books of a Library cannot exist without the Library. So Book and\n' +
-                'Library are related by Composition.',
+            'correctResponse': 'No. Java does not allow overriding a static method. If you create a\n' +
+                'static method with same name in subclass, then it is a new method,\n' +
+                'not an overridden method.\n' +
+                'To override a method, you need an instance of a class. Static method\n' +
+                'is not associated with any instance of the class. So the concept of\n' +
+                'overriding does not apply here.',
             'incorrectResponse': ''
         },
         {
-            'q': 'Is it possible to use this() and\n' +
-                'super() both in same constructor?',
+            'q': 'What is the difference between method overloading and method\n' +
+                'overriding in Java?\n',
             'options': [
                 'Show answer'
             ],
             'correctIndex': 0,
-            'correctResponse': 'No, Java does not allow using both super() and this() in same\n' +
-                'constructor. As per Java specification, super() or this() must be the\n' +
-                'first statement in a constructor.',
+            'correctResponse': '1. Method overloading is static polymorphism. Method\n' +
+                'overriding is runtime polymorphism.\n' +
+                '2. Method overloading occurs within the same class. Method\n' +
+                'overriding happens in two classes with hierarchy\n' +
+                'relationship.\n' +
+                '3. Parameters must be different in method overloading.\n' +
+                'Parameters must be same in method overriding.\n' +
+                '4. Method overloading is a compile time concept. Method\n' +
+                'overriding is a runtime concept.',
             'incorrectResponse': ''
         },
         {
-            'q': 'What is the meaning of object cloning in Java?\n',
+            'q': 'What is Runtime Polymorphism?\n',
             'options': [
                 'Show answer'
             ],
             'correctIndex': 0,
-            'correctResponse': 'Object.clone() method is used for creating an exact copy of the\n' +
-                'object in Java. It acts like a copy constructor. It creates and returns\n' +
-                'a copy of the object, with the same class and with all the fields\n' +
-                'having same values as of the original object.\n\n' +
-                'One disadvantage of cloning is that the return type is an Object. It\n' +
-                'has to be explicitly cast to actual type.',
+            'correctResponse': 'Runtime Polymorphism or Dynamic Polymorphism is the\n' +
+                'polymorphism that exists at runtime. In case of method overriding it\n' +
+                'is not known which method will be called at runtime. Based on the\n' +
+                'type of object, JVM decides the exact method that should be called.\n' +
+                'So at compile time it is not known which method will be called at\n' +
+                'run time.',
             'incorrectResponse': ''
         },
         {
-            'q': 'Why it is not a good practice to\n' +
-                'create static variables in Java?',
+            'q': 'Explain the difference between static and dynamic binding?\n',
             'options': [
                 'Show answer'
             ],
             'correctIndex': 0,
-            'correctResponse': 'Static variables are common to all the objects of a class. If a new\n' +
-                'object is created, there is no need to test the value of static variable.\n' +
-                'Any code that uses static variable can be in any state. It can be\n' +
-                'within a new object or at a class level. So the scope of static\n' +
-                'variable is open ended in a Java class.\n\n' +
-                'If we want tighter control on scope, then variables should be\n' +
-                'created at the object creation level.\n\n' +
-                'Also defining static variables is not a good practice because they go\n' +
-                'against the principles of Object Oriented Programming.',
+            'correctResponse': 'In Static binding references are resolved at compile time. In\n' +
+                'Dynamic binding references are resolved at Run time.\n\n' +
+                'E.g.\n' +
+                'Person p = new Person();\n' +
+                'p.walk(); // Java compiler resolves this binding at compile time.\n\n' +
+                'public void walk(Object o){\n' +
+                '((Person) o).walk(); // this is dynamic binding.\n' +
+                '}',
             'incorrectResponse': ''
         }
     ]
