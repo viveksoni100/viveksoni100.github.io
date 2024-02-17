040b098878c02a5d27bf4dee34f89b179b8cebf1
viveksoni100 <viveksoni100@gmail.com>
2024-02-17 22:08:54 +0530

    Quiz#10 added

--- js/quiz.js
+++ js/quiz.js
@@ -2,110 +2,110 @@ $('#quiz').quiz({
     counterFormat: 'Question %current of %total',
     questions: [
         {
-            'q': 'Which of the following modifiers can be applied to an abstract method?',
+            'q': 'Which statement about static interface methods are correct?',
             'options': [
-                'A.final',
-                'B.private',
-                'C.public',
-                'D.default'
-            ],
-            'correctIndex': 2,
-            'correctResponse': 'Correct ✅',
-            'incorrectResponse': 'C. An abstract method cannot include the final or private modifier. If a method\n' +
-                'contained either of these modifiers, then no concrete subclass would ever be able to override\n' +
-                'it with an implementation. For these reasons, options A and B are incorrect. Option D is also\n' +
-                'incorrect because the default keyword applies to concrete interface methods, not abstract\n' +
-                'methods.'
-        },
-        {
-            'q': 'What is the result of compiling and executing the following class?\n\n' +
-                '1: public class ParkRanger {\n' +
-                '2: int birds = 10;\n' +
-                '3: \tpublic static void main(String[] data) {\n' +
-                '4: \t\tvar trees = 5;\n' +
-                '5: \t\tSystem.out.print(trees+birds);\n' +
-                '6: \t}\n' +
-                '7: }',
-            'options': [
-                'A.It compiles and outputs 5.',
-                'B.It compiles and outputs 15.',
-                'C.It does not compile.',
-                'D.It compiles but throws an exception at runtime.'
-            ],
-            'correctIndex': 2,
-            'correctResponse': 'Good ✅',
-            'incorrectResponse': 'C. The code does not compile because of line 5, making option C the correct answer.\n' +
-                'For this question, it helps to understand variable scope. The main() method is static and does not\n' +
-                'have access to any class instance variables. The birds variable is not static and requires\n' +
-                'a class instance variable to access. Therefore, the code does not compile when the static\n' +
-                'method attempts to access a non-static variable without an instance of the class.'
-        },
-        {
-            'q': 'Which set of modifiers, when added to a default method within an interface, prevents\n' +
-                'it from being overridden by a class implementing the interface?',
-            'options': [
-                'A.const',
-                'B.final',
-                'C.static',
-                'D.private',
-                'E.private static',
-                'F.None of the above'
-            ],
-            'correctIndex': 5,
-            'correctResponse': 'Excellent ✅',
-            'incorrectResponse': 'F. There is no modifier that can prevent a default method from being overridden\n' +
-                'in a class implementing an interface, making option F correct.'
-        },
-        {
-            'q': 'Given the following application, fill in the missing values in the table starting\n' +
-                'from the top and going downward.' +
-                'package competition;\n' +
-                'public class Robot {' +
-                '\tstatic String weight = "A lot";\n' +
-                '\tdouble ageMonths = 5, ageDays = 2;\n' +
-                '\tprivate static boolean success = true;\n\n' +
-                '\tpublic void main(String[] args) {\n' +
-                '\t\tfinal String retries = "1";\n' +
-                '\t\t// P1\n'+
-                '\t\t}\n' +
-                '\t}\n\n' +
-                '| Variable  | TypeNumber of Variables Accessible at P1 |\n' +
-                '| Class       | __________                                                            |\n' +
-                '| Instance | __________                                                            |\n' +
-                '| Local       | __________                                                            |\n',
-            'options': [
-                'A.2,0,1',
-                'B.2,2,1',
-                'C.1,0,1',
-                'D.0,2,1'
+                'A.A static interface method can be final',
+                'B.A static interface method can be declared private',
+                'C.A static interface method can be package-private',
+                'D.A static interface method can be declared protected'
             ],
             'correctIndex': 1,
-            'correctResponse': 'Bravo ✅',
-            'incorrectResponse': 'B. Notice in this question that main() is not a static method; therefore,\n' +
-                'it can access both class and instance variables. Since there are two class variables and two instance\n' +
-                'variables defined, option B is the correct answer.'
+            'correctResponse': 'Correct ✅',
+            'incorrectResponse': 'B. Option A is incorrect as methods cannot be marked final within an interface.\n' +
+                'Interfaces support static methods that are marked public or private, making option B correct.'
         },
         {
-            'q': 'Given the following code, what values inserted, in order, into the blank lines allow\n' +
-                'the code to compile?\n\n' +
-                '_______ agent;\n' +
-                'public _______ Banker {\n' +
-                '\tprivate static _______ getMaxWithdrawal() {\n' +
-                '\t\treturn 10;\n' +
+            'q': 'Fill in the blanks with the only option that makes this statement false:\n' +
+                'A(n) _______ can access _______ of the enclosing class in which it is defined.',
+            'options': [
+                'A.static nested class, static members',
+                'B.static nested class, instance members',
+                'C.member inner class, static members',
+                'D.member inner class, instance members',
+                'E.local class, instance members from within an instance method',
+                'F.anonymous class, instance members from within an instance method'
+            ],
+            'correctIndex': 1,
+            'correctResponse': 'Good ✅',
+            'incorrectResponse': 'A static nested class cannot access instance members of the enclosing class, \n' +
+                'making option B correct. The rest of the options form true statements.'
+        },
+        {
+            'q': 'What is the result of executing the following program?\n\n' +
+            'public class Canine {\n' +
+            '\tpublic String woof(int bark) {\n' +
+            '\t\treturn "1"+bark.toString();\n'+
+            '\t}\n\n' +
+            '\tpublic String woof(Integer bark) {\n' +
+            '\t\treturn "2"+bark.toString();\n' +
+            '\t}\n\n' +
+            '\tpublic String woof(Object bark) {\n' +
+            '\t\treturn "3"+bark.toString();\n' +
+            '\t}\n\n' +
+            '\tpublic static void main(String[] a) {\n' +
+            '\t\tSystem.out.println(woof((short)5));\n' +
+            '\t}\n' +
+            '}',
+            'options': [
+                'A.15',
+                'B.25',
+                'C.35',
+                'D.One line does not compile',
+                'E.Two lines do not compile',
+                'F.The program compiles but throws an exception at runtime.'
+            ],
+            'correctIndex': 4,
+            'correctResponse': 'Excellent ✅',
+            'incorrectResponse': 'E. The first woof() method does not compile because bark is a primitive, not an object,\n' +
+                'and does not have a toString() method. The main() method also does not com-\n' +
+                'pile because it is static and all of the woof() methods require an instance of Canine.\n' +
+                'Since these two lines do not compile, option E is the correct answer. If the toString()\n' +
+                'was removed from the first method and all of the methods were marked static, then the\n' +
+                'program would print 15 at runtime.'
+        },
+        {
+            'q': 'What statement best describes the notion of effectively final in Java?',
+            'options': [
+                'A.A local variable that is marked final',
+                'B.A static variable that is marked final',
+                'C.A local variable whose primitive value or object reference does not changes after it is initialized',
+                'D.A local variable whose primitive value or object reference does not change after\n'
+                + 'a certain point in the method',
+                'E.None of te above'
+            ],
+            'correctIndex': 2,
+            'correctResponse': 'Bravo ✅',
+            'incorrectResponse': 'C. A local variable is effectively final when its primitive value or object reference does not\n' +
+                'change after it is initialized, making option C the correct answer. Note that option D is incor-\n' +
+                'rect because any change to the variable after it is initialized disqualifies it for being consid-\n' +
+                'ered effectively final.'
+        },
+        {
+            'q': 'What is the output of the Turnip class?\n\n' +
+                'package animal;\n' +
+                'interface GameItem {\n' +
+                '\tint sell();\n' +
+                '}\n' +
+                'abstract class Vegetable implements GameItem {\n' +
+                '\tpublic final int sell() { return 5; }\n' +
+                '}\n' +
+                'public class Turnip extends Vegetable {\n' +
+                '\tpublic final int sell() { return 3; }\n' +
+                '\tpublic static void main(String[] expensive){\n' +
+                '\t\tSystem.out.println(new Turnip().sell());\n' +
                 '\t}\n' +
                 '}',
             'options': [
-                'A.package, new, int',
-                'B.package, class, long',
-                'C.import, class, null',
-                'D.import, interface, void'
+                'A.3',
+                'B.5',
+                'C.The code does not compile.',
+                'D.The code compiles but throws an exception at runtime'
             ],
-            'correctIndex': 1,
+            'correctIndex': 2,
             'correctResponse': 'Perfect ✅',
-            'incorrectResponse': 'B. Option A is incorrect because new cannot be used to declare a type.\n' +
-                'Option C is incorrect because null is a literal and cannot be used as the name of a class.\n' +
-                'That leaves options B as the correct answers. Note that 10 can be returned as an int or implicitly\n' +
-                'promoted to a long, without issue.'
+            'incorrectResponse': 'C. The sell() method is declared final in the Vegetable class. The Turnip class\n' +
+                'then attempts to override this method, resulting in a compilation error, making option C the\n' +
+                'correct answer.'
         }
     ]
 });
